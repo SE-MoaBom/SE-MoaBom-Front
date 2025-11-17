e9cb646e2fbe0694ad56e7b7677f2f6a8862873b
KIM SEUNGIL <ksi010609@gmail.com>
2025-11-17 23:27:59 +0900

    feat: 내가 찜한 리스트 UI 구현

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { WishlistProvider } from "./contexts/WishlistContext.tsx"; // WishlistProvider 불러오기
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* 2. <App /> 컴포넌트를 <WishlistProvider>로 감싸주기 */}
+    <WishlistProvider>
+      <App />
+    </WishlistProvider>
   </React.StrictMode>
 );
